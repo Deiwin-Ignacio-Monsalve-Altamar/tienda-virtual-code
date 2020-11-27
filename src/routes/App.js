d58a4ea5bd2c2fd0c20ba38aa72e39ca136efeea
Deiwin Monsalve <deiwin1987@gmail.com>
2020-11-27 00:01:42 -0500

    todas las rutas del proyecto

--- src/routes/App.js
+++ src/routes/App.js
@@ -1,26 +1,28 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import '../assets/styles/App.css'
+import '../assets/styles/global.css';
 
 import Home from '../containers/Home';
-/* import Login from '../containers/Login';
-import Register from '../containers/Register';
- */
 //import NotFound from '../containers/NotFound';
 import Contacts from '../containers/Contacts';
-//import Layout from '../components/Layout';
+import SingUp from '../containers/SingIn';
+import Men from '../containers/Men';
+import Layout from '../components/Layout';
 
 const App = () => (
   <BrowserRouter>
-{/*     <Layout> */}
+    <Layout>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/contact" component={Contacts} />
+        <Route exact path="/login" component={SingUp} />
+        <Route exact path="/men" component={Men} />
         {/* <Route exact path="/register" component={Register} />
         <Route exact path="/player/:id" component={Player} />
         <Route component={NotFound} /> */}
       </Switch>
-{/*     </Layout> */}
+    </Layout>
   </BrowserRouter>
 )
 
