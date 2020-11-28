f56b713c59fa98b1d930972b2523bb925a6c318a
Deiwin Monsalve <deiwin1987@gmail.com>
2020-11-27 22:35:11 -0500

    agregando layout para el header y footer del sitio

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
-import Search from './Search';
-import Categories from './Categories';
-import MultiCarouselPage from './MultiCarouselPage';
 
 const Layout = ({ children }) => (
   <div className="App">
     <Header />
-    <Search />
-    <Categories />
         {children}
-    <MultiCarouselPage />
     <Footer />
   </div>
 );
